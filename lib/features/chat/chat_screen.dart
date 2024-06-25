e68be14cb664f05e52682112335a6765b8cc7ec9
jchillah <jchillah@gmail.com>
2024-06-25 17:25:01 +0200

    finished chat screen, implemented an icon button in App bar to start sendmessages, and Streambuilder to show messages

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:streams_exercises/features/chat/chat_message.dart';
 import 'package:streams_exercises/features/chat/chat_repository.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     super.key,
     required this.chatRepository,
@@ -10,14 +11,67 @@ class ChatScreen extends StatelessWidget {
   final ChatRepository chatRepository;
 
   @override
+  ChatScreenState createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat Screen'),
+        actions: [
+          const Spacer(),
+          IconButton(
+            icon: const Icon(Icons.play_arrow),
+            onPressed: () {
+              widget.chatRepository.startSendingMessages();
+            },
+          ),
+          const Spacer(),
+        ],
       ),
-      body: const Center(
-        child: Text("Hier sollen die Chatnachrichten stehen"),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<ChatMessage>(
+              stream: widget.chatRepository.messages,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(child: Text('Error loading messages'));
+                } else if (!snapshot.hasData) {
+                  return const Center(child: Text('No messages yet'));
+                } else {
+                  final messages = <ChatMessage>[];
+                  if (snapshot.hasData) {
+                    messages.add(snapshot.data!);
+                  }
+                  return ListView.builder(
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          child: Text(messages[index].user[0]),
+                        ),
+                        title: Text(messages[index].user),
+                        subtitle: Text(messages[index].message),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    widget.chatRepository.dispose();
+    super.dispose();
   }
 }
